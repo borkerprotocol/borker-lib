02e89792ccf47f45a7ada558c9bbce652f9f52fe
Aiden McClelland <gagglehoof@gmail.com>
2018-11-14 23:12:31 -0700

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env::args;
+
+macro_rules! map_str {
+    ($x:expr) => {
+        $x.as_ref().map(|s| s.as_str())
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cmd: Vec<String> = args().collect();
+    let name = cmd.get(0).unwrap();
+    match map_str!(cmd.get(1)) {
+        Some("hello") => println!("Hello!"),
+        None | Some("help") => println!("usage: {} <command> [args...]", name),
+        Some(a) => println!("'{}' is not a valid command", a),
+    }
 }
