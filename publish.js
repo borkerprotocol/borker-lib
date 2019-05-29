2f079420bbab78926fe9559ce9e152d1b5f526a0
Aiden McClelland <gagglehoof@gmail.com>
2019-05-28 19:56:28 -0600

    v10

--- publish.js
+++ publish.js
@@ -1,7 +1,11 @@
-import * as fs from 'fs'
-import * as child_process from 'child_process'
+fs = require('fs')
+child_process = require('child_process')
 
-if (process.argv[2] !== 'node' && process.argv[2] !== 'browser') {
+if (process.argv[2] === 'node') {
+    child_process.execSync('wasm-pack build --target=nodejs')
+} else if (process.argv[2] === 'browser') {
+    child_process.execSync('wasm-pack build --target=browser')
+} else {
     throw new Error("must be 'node' or 'browser'")
 }
 fs.writeFileSync('./pkg/borker_rs.d.ts', fs.readFileSync('./types/borker_rs.d.ts'))
