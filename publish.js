13747f070f232c55df4d0aadd488299f2c753d4a
Aiden McClelland <gagglehoof@gmail.com>
2019-05-28 20:03:14 -0600

    v10

--- publish.js
+++ publish.js
@@ -2,14 +2,15 @@ fs = require('fs')
 child_process = require('child_process')
 
 if (process.argv[2] === 'node') {
-    child_process.execSync('wasm-pack build --target=nodejs')
+    child_process.execSync('wasm-pack build --target=nodejs', { stdio: "inherit" })
 } else if (process.argv[2] === 'browser') {
-    child_process.execSync('wasm-pack build --target=browser')
+    child_process.execSync('wasm-pack build --target=browser', { stdio: "inherit" })
 } else {
     throw new Error("must be 'node' or 'browser'")
 }
 fs.writeFileSync('./pkg/borker_rs.d.ts', fs.readFileSync('./types/borker_rs.d.ts'))
+fs.appendFileSync('./pkg/borker_rs.js', fs.readFileSync('./types/borker_enums_' + process.argv[2] + '.js'))
 pkg = require('./pkg/package.json')
 pkg.name += '-' + process.argv[2]
 fs.writeFileSync('./pkg/package.json', JSON.stringify(pkg, null, 2))
-child_process.execSync('cd pkg && npm publish')
+child_process.execSync('cd pkg && npm publish', { stdio: "inherit" })
